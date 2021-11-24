fb5ade1bc0110ed451210f4c588337c516ed3bd6
seguriki <joselorca_92@hotmail.com>
2021-11-24 13:48:38 +0100

    Primera versión main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/listar")
+def listar():
+    f = open("contactos.txt", "r")
+    return {f.read()}
+
+@app.post("/alta/{nombre}/{telefono}")
+def dar_alta(nombre: str, telefono: str):
+    f = open("contactos.txt", "w")
+    f.write("{\"nombre\":\"" + nombre + "\",\"telefono\":\"" + telefono + "\"}")
+    f.close()
+    return {"alta":"ok"}
